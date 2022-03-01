dd04d9c6779e89b4f1f0d926c644a12f70116280
soblab <soblab@Whit209-01.it.farmingdale.edu>
2022-03-01 14:44:02 -0500

    add freshman and senior class

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -17,5 +17,33 @@ public class Student {
 	// ToDo 3: Add a field for GPA and create setter and getter
 	
 	// ToDo 4: Add comments to your code
-
+	private String name;
+	private int age;
+        private int gpa;
+	
+	public Student(String name, int age, int gpa) {
+		this.name = name;
+		this.age = age;
+                this.gpa = gpa;
+	}
+	
+	public String getName() {
+		return name;
+	}
+        public int getAge() {
+		return age;
+	}
+        public int getGpa() {
+		return gpa;
+	}
+        
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+        public void setgpa(int gpa) {
+		this.gpa = gpa;
+	}
 }
